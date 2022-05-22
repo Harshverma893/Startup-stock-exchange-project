679f82e1bf8b29ff2558c06f7bae0ce1bfecd3e2
Nishant <ngw334@gmail.com>
2022-05-22 11:24:45 +0530

    signup 2.3

--- app/src/main/java/com/example/startupstockexchange/MainActivity.java
+++ app/src/main/java/com/example/startupstockexchange/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnNext(View view) {
-        Intent intent= new Intent(this, MainActivity2.class);
+        Intent intent= new Intent(this, SignUpActivity.class);
         startActivity(intent);
     }
 }
