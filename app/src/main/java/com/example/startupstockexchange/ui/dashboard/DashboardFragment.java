1583e46a30fb340892b1a17c70779984436f49d7
Nishant <ngw334@gmail.com>
2022-05-27 20:15:12 +0530

    Search navigation added

--- app/src/main/java/com/example/startupstockexchange/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/startupstockexchange/ui/dashboard/DashboardFragment.java
@@ -12,24 +12,5 @@ import com.example.startupstockexchange.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
 
-private FragmentDashboardBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
-
-    binding = FragmentDashboardBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
-
-//        final TextView textView = binding.textDashboard;
-//        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
-    }
-
-@Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
 }
---
+++ app/src/main/java/com/example/startupstockexchange/ui/search/SearchFragment.java
@@ -0,0 +1,7 @@
+package com.example.startupstockexchange.ui.search;
+
+import androidx.fragment.app.Fragment;
+
+public class SearchFragment extends Fragment {
+
+}
